21d42baf9b9754a49cb8c73c08afa8fca7a7f966
Marian Mocanu <mocanumarian1993@gmail.com>
2019-10-03 17:56:24 +0200

    gitignore

--- EchoServer/src/EchoServer.java
+++ EchoServer/src/EchoServer.java
@@ -17,6 +17,8 @@ public class EchoServer {
         while (true) {
             try (
                     ServerSocket serverSocket = new ServerSocket(port);
+                    //
+
                     Socket clientSocket = serverSocket.accept();
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
